f7d5f474c45320bb7d8f3b9ee3faf1591f2ec9c1
Maziar Rud <m.mansoury@live.com>
2017-08-30 15:39:49 +0430

    Fixed Image Loading through file-loader(solved:Downgrade loader version)

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ const isProd = process.env.NODE_ENV = 'production';
 const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
 const cssProd = ExtractTextPlugin.extract({
     fallback: 'style-loader',
-    loader: ['css-loader', 'sass-loader'],
-    publicPath: './dist'
+    use: ['css-loader', 'sass-loader'],
+    publicPath: '../',
 });
 
 const cssConfig = isProd ? cssProd : cssDev;
@@ -21,10 +21,11 @@ module.exports = {
     entry: "./src/js/main.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        filename: "js/[name].bundle.js"
     },
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.js$/,
                 use: ["babel-loader"],
                 exclude: path.resolve(__dirname, "node_modules")
@@ -33,16 +34,20 @@ module.exports = {
                 test: /\.css$/,
                 use: cssConfig,
                 exclude: path.resolve(__dirname, "node_modules")
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: ['file-loader?name=images/[hash:12].[ext]','image-webpack-loader']
             }
         ]
     },
     plugins: [
         HtmlWebpackPluginConfig,
-        // new webpack.optimize.UglifyJsPlugin()
+        // new webpack.optimize.UglifyJsPlugin(),
         new ExtractTextPlugin({
-            filename: "bundle.css",
-            disable: false,
+            filename: 'css/[name].bundle.css',
+            disable: !isProd,
             allChunks: true
-        }),
+        })
     ]
 }
