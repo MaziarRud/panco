872cf76569e60eda929116e54f0bbcfbeedaa726
Maziar Rud <m.mansoury@live.com>
2017-08-30 10:33:26 +0430

    add Extract Text Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,48 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    title:"Panco",
-    template:"./src/index.ejs"
+    title: "Panco",
+    template: "./src/index.ejs"
 });
+
+const isProd = process.env.NODE_ENV = 'production';
+const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+const cssProd = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    loader: ['css-loader', 'sass-loader'],
+    publicPath: './dist'
+});
+
+const cssConfig = isProd ? cssProd : cssDev;
 
 module.exports = {
     entry: "./src/js/main.js",
     output: {
-        path:path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
-    module:{
-        rules:[
-            {test: /\.js$/, use: ["babel-loader"], exclude: path.resolve(__dirname, "node_modules") },
-            {test: /\.css$/, use: ["style-loader","css-loader"], exclude: path.resolve(__dirname, "node_modules")}
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: ["babel-loader"],
+                exclude: path.resolve(__dirname, "node_modules")
+            },
+            {
+                test: /\.css$/,
+                use: cssConfig,
+                exclude: path.resolve(__dirname, "node_modules")
+            }
         ]
     },
-    plugins:[
+    plugins: [
         HtmlWebpackPluginConfig,
         // new webpack.optimize.UglifyJsPlugin()
+        new ExtractTextPlugin({
+            filename: "bundle.css",
+            disable: false,
+            allChunks: true
+        }),
     ]
 }
